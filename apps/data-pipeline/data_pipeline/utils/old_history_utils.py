2a560f3a8672cac0e2d7e1928053554d6aeb933b
ma9o <magogagiovanni@gmail.com>
2024-07-02 20:24:21 +0200

    [data-pipeline] Make InterestsGenerator functional

--- apps/data-pipeline/data_pipeline/utils/old_history_utils.py
+++ apps/data-pipeline/data_pipeline/utils/old_history_utils.py
@@ -34,149 +34,126 @@ class FullHistorySessionsOutput:
     count_invalid_responses: int
 
 
-@dataclass
-class InterestsGenerator:
-    daily_dfs: Dict[datetime.date, pl.DataFrame]
-    first_instruction: str
-    second_instruction: str
-    llm: LLM
-    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
-    sampling_params: SamplingParams
-    chunk_size: int = 15
+def generate_chunks(daily_dfs: Dict[datetime.date, pl.DataFrame], chunk_size: int = 15):
+    chunks: Dict[datetime.date, List[pl.DataFrame]] = {}
 
-    def _extract_interests_list(self, text: str) -> Optional[List[str]]:
-        match = re.search(r"\[(.*?)\]", text)
-        if match:
-            # If a match is found, split the substring by semicolon
-            interests = match.group(1).replace('"', "").replace("'", "").split(";")
-            return [interest.strip() for interest in interests]
+    for date, day_df in daily_dfs.items():
+        chunks[date] = []
+        for slice in day_df.iter_slices(chunk_size):
+            chunks[date].append(slice.select("hour", "title"))
+
+    return chunks
+
+
+def extract_interests_list(text: str) -> Optional[List[str]]:
+    match = re.search(r"\[(.*?)\]", text)
+    if match:
+        # If a match is found, split the substring by semicolon
+        interests = match.group(1).replace('"', "").replace("'", "").split(";")
+        return [interest.strip() for interest in interests]
+    else:
         return None
 
-    def _generate_chunks(self):
-        self.chunks: dict[datetime.date, List[pl.DataFrame]] = {}
 
-        for date, day_df in self.daily_dfs.items():
-            self.chunks[date] = []
-            for slice in day_df.iter_slices(self.chunk_size):
-                self.chunks[date].append(slice.select("hour", "title"))
+def vllm_generate(
+    llm: LLM,
+    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+    conversations: List[List[Dict[str, str]]],
+):
+    # TODO: How do we print the progress bar?
+    return llm.generate(
+        tokenizer.apply_chat_template(
+            conversations,
+            tokenize=False,
+            add_generation_prompt=True,  # type: ignore
+        ),
+        # TODO: We could potentially make this part of the Config so the params can be
+        # configured from the Dagster UI
+        SamplingParams(temperature=0.8, top_p=0.95, max_tokens=1024),
+    )
 
-    def _generate_chunked_interests(self):
-        """
-        TODO: Experiment with prefix caching. See the example below.
-        https://github.com/vllm-project/vllm/blob/main/examples/offline_inference_with_prefix.py
-        """
-        self.dates = []
-        conversations: List[List[Dict[str, str]]] = []
 
-        for date, day_dfs in self.chunks.items():
-            for _, frame in enumerate(day_dfs, start=1):
-                # Set Polars' string formatting so none of the rows or strings are compressed / cut off.
-                max_chars = frame["title"].str.len_chars().max()
+def generate_chunked_interests(
+    ml_model_name: str,
+    chunks: Dict[datetime.date, List[pl.DataFrame]],
+    first_instruction: str,
+    second_instruction: str,
+):
+    dates = []
+    conversations: List[List[Dict[str, str]]] = []
 
-                with pl.Config(
-                    tbl_formatting="NOTHING",
-                    tbl_hide_column_data_types=True,
-                    tbl_hide_dataframe_shape=True,
-                    fmt_str_lengths=max_chars,
-                    tbl_rows=-1,
-                ):
-                    self.dates.append(date)
-                    conversations.append(
-                        [
-                            {
-                                "role": "user",
-                                "content": f"{self.first_instruction}\n{frame}",
-                            }
-                        ]
-                    )
+    for date, day_dfs in chunks.items():
+        for _, frame in enumerate(day_dfs, start=1):
+            dates.append(date)
+            conversations.append(
+                [
+                    {
+                        "role": "user",
+                        "content": f"{first_instruction}\n{frame}",
+                    }
+                ]
+            )
 
-        # TODO: How do we print the progress bar?
-        first_requests = self.llm.generate(
-            self.tokenizer.apply_chat_template(
-                conversations,
-                tokenize=False,
-                add_generation_prompt=True,  # type: ignore
-            ),
-            self.sampling_params,
-        )
-        first_responses = [resp.outputs[0].text for resp in first_requests]
+    logger = get_dagster_logger()
+    logger.info(f"Loading {ml_model_name}...")
 
-        for c, r1 in zip(conversations, first_responses):
-            c.append({"role": "assistant", "content": r1})
-            c.append({"role": "user", "content": self.second_instruction})
+    llm = LLM(ml_model_name)
+    tokenizer = AutoTokenizer.from_pretrained(ml_model_name)
 
-        second_requests = self.llm.generate(
-            self.tokenizer.apply_chat_template(
-                conversations,
-                tokenize=False,
-                add_generation_prompt=True,  # type: ignore
-            ),
-            self.sampling_params,
-        )
-        second_responses = [resp.outputs[0].text for resp in second_requests]
+    first_requests = vllm_generate(llm, tokenizer, conversations)
+    first_responses = [resp.outputs[0].text for resp in first_requests]
 
-        for c, r2 in zip(conversations, second_responses):
-            c.append({"role": "assistant", "content": r2})
+    for c, r1 in zip(conversations, first_responses):
+        c.append({"role": "assistant", "content": r1})
+        c.append({"role": "user", "content": second_instruction})
 
-        self.chunked_interests = [
-            self._extract_interests_list(resp) for resp in second_responses
-        ]
+    second_requests = vllm_generate(llm, tokenizer, conversations)
+    second_responses = [resp.outputs[0].text for resp in second_requests]
 
-        # Save the convo for each chunk as a single string
-        self.chunked_convos = [
-            "\n".join([f"{c['role']}: {c['content']}" for c in convo])
-            for convo in conversations
-        ]
+    for c, r2 in zip(conversations, second_responses):
+        c.append({"role": "assistant", "content": r2})
 
-    def generate_output_records(self):
-        self._generate_chunks()
-        self._generate_chunked_interests()
+    chunked_interests = [extract_interests_list(resp) for resp in second_responses]
 
-        for date, chunked_interests, chunked_convos in zip(
-            self.dates, self.chunked_interests, self.chunked_convos
-        ):
-            chunked_interests = chunked_interests or []
+    # Save the convo for each chunk as a single string
+    chunked_convos = [
+        "\n".join([f"{c['role']}: {c['content']}" for c in convo])
+        for convo in conversations
+    ]
 
-            chunked_interests = [interest for interest in chunked_interests if interest]
+    for date, interests, convos in zip(dates, chunked_interests, chunked_convos):
+        interests = [interest for interest in (interests or []) if interest]
 
-            yield {
-                "date": date,
-                "chunked_convos": chunked_convos,
-                "interests": chunked_interests,
-                "count_invalid_responses": 1 if len(chunked_interests) == 0 else 0,
-            }
+        yield {
+            "date": date,
+            "chunked_convos": convos,
+            "interests": interests,
+            "count_invalid_responses": 1 if len(interests) == 0 else 0,
+        }
 
 
 def get_full_history_sessions(
-    daily_dfs: dict[datetime.date, pl.DataFrame],
+    full_takeout: pl.DataFrame,
+    ml_model_name: str,
     chunk_size: int,
     first_instruction: str,
     second_instruction: str,
-    ml_model_name: str,
 ):
     logger = get_dagster_logger()
 
-    logger.info("Loading the model. This may take a few minutes...")
-    llm = LLM(model=ml_model_name)
-    tokenizer = AutoTokenizer.from_pretrained(ml_model_name)
-
-    # TODO: We could potentially make this part of the Config so the params can be
-    # configured from the Dagster UI
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=1024)
+    # Split into multiple data frames (one per day). This is necessary to correctly
+    # identify the data associated with each time entry.
+    daily_dfs = full_takeout.with_columns(
+        date=pl.col("timestamp").dt.date()
+    ).partition_by("date", as_dict=True, include_key=False)
 
     logger.info(f"Processing {len(daily_dfs)} records")
 
-    interests_generator = InterestsGenerator(
-        daily_dfs=daily_dfs,
-        first_instruction=first_instruction,
-        second_instruction=second_instruction,
-        llm=llm,
-        tokenizer=tokenizer,
-        sampling_params=sampling_params,
-        chunk_size=chunk_size,
-    )
+    chunks = generate_chunks(daily_dfs, chunk_size)
 
-    daily_records = interests_generator.generate_output_records()
+    daily_records = generate_chunked_interests(
+        ml_model_name, chunks, first_instruction, second_instruction
+    )
 
     output_df = (
         pl.DataFrame(daily_records)
