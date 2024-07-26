d4b977de5f20010e10764326fbf6ed2cb2117651
ma9o <magogagiovanni@gmail.com>
2024-07-26 11:47:22 +0200

    [data-pipeline] Cleanup

--- apps/data-pipeline/data_pipeline/utils/postgres.py
+++ apps/data-pipeline/data_pipeline/utils/postgres.py
@@ -1,8 +1,4 @@
-import polars as pl
-import psycopg
 from cuid2 import Cuid
-from dagster import get_dagster_logger
-from psycopg import sql
 from sqlalchemy import make_url
 
 CUID_GENERATOR = Cuid(length=25)
@@ -21,38 +17,3 @@ def conn_string_to_conn_args(conn_string: str):
 
 def generate_cuid():
     return CUID_GENERATOR.generate()
-
-
-# TODO: Handle conflicts
-def insert_dataframe_to_table(
-    df: pl.DataFrame, conn: psycopg.Connection, table_name: str
-) -> list:
-    # Convert Polars DataFrame to list of tuples
-    data = df.to_numpy().tolist()
-    columns = df.columns
-
-    # Create SQL query for inserting data with conflict handling
-    column_names = sql.SQL(", ").join(map(sql.Identifier, columns))
-    placeholders = sql.SQL(", ").join(sql.Placeholder() * len(columns))
-
-    logger = get_dagster_logger()
-    logger.info(f"Inserting {len(data)} rows of {column_names} into {table_name}")
-
-    insert_query = sql.SQL(
-        """
-        INSERT INTO {table} ({columns})
-        VALUES ({values})
-        RETURNING id
-    """
-    ).format(
-        table=sql.Identifier(table_name),
-        columns=column_names,
-        values=placeholders,
-    )
-
-    # Execute the query and fetch the inserted IDs
-    with conn.cursor() as cur:
-        cur.executemany(insert_query, data)
-        inserted_ids = [row[0] for row in cur.fetchall()]
-
-    return inserted_ids
