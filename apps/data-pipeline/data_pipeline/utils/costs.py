a70741261cb9efe44045bfa2dccd70abddfbce04
ma9o <magogagiovanni@gmail.com>
2024-08-18 10:40:50 +0100

    snake

--- apps/data-pipeline/data_pipeline/utils/costs.py
+++ apps/data-pipeline/data_pipeline/utils/costs.py
@@ -1,11 +1,11 @@
 import time
 
-import torch
-
 A100_HOURLY_COST = 3.67
 
 
 def get_gpu_runtime_cost(start_time: float) -> float:
+    import torch
+
     num_gpus = torch.cuda.device_count()
     runtime_s = time.time() - start_time
     return runtime_s / 3600 * A100_HOURLY_COST * num_gpus
