7903bf47e1b99a0d03de54fd64af4b9f692e9574
ma9o <magogagiovanni@gmail.com>
2024-08-19 22:04:51 +0100

    [data-pipeline] Update spot cost for logging

--- apps/data-pipeline/data_pipeline/utils/costs.py
+++ apps/data-pipeline/data_pipeline/utils/costs.py
@@ -1,14 +1,11 @@
-import json
 import time
 
-from data_pipeline.utils.capabilities import gpu_info
-
-A100_HOURLY_COST = 3.67
+NC48ads_A100_v4_HOURLY_COST_SPOT = 0.8889
 
 
 def get_gpu_runtime_cost(start_time: float) -> float:
     runtime_s = time.time() - start_time
     try:
-        return runtime_s / 3600 * A100_HOURLY_COST * len(json.loads(gpu_info()))
+        return runtime_s / 3600 * NC48ads_A100_v4_HOURLY_COST_SPOT
     except Exception:
         return 0.0
