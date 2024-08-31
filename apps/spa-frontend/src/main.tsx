a7d9166bd31860a10b3b461a9fe68d49060294b6
ma9o <magogagiovanni@gmail.com>
2024-08-31 17:25:28 +0100

    [spa-frontend] sentry

--- apps/spa-frontend/src/main.tsx
+++ apps/spa-frontend/src/main.tsx
@@ -6,7 +6,23 @@ import { trpc, trpcClient } from './app/utils/trpc';
 
 import { App } from './app/app';
 
+import * as Sentry from '@sentry/react';
 import './styles.css';
+
+Sentry.init({
+  dsn: 'https://3ef231b526e7fd50c806ceadc46235bd@o4507872863977472.ingest.us.sentry.io/4507872867909632',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [/^https:\/\/api\.enclaveid\.com/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
