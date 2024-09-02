147de389806a6bdcff9bb2cfd64f7465ef1c4653
ma9o <magogagiovanni@gmail.com>
2024-09-02 13:20:58 +0100

    [data-pipeline] hook?

--- apps/data-pipeline/data_pipeline/hooks/api_hooks.py
+++ apps/data-pipeline/data_pipeline/hooks/api_hooks.py
@@ -7,6 +7,8 @@ from dagster import (
 
 @success_hook(name="notify_api_on_success")
 def notify_api_on_success(context: HookContext):
+    context.log.info(context.op.tags)
+    context.log.info(context.op)
     if context.op.tags.get("hook") == "notify_api_on_success":
         response = requests.post(
             "https://api.enclaveid.com/webhooks/pipeline-finished",
