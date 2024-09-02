6ffea6d2398ace1b6b7cb209f2d3d0de7f77813b
ma9o <magogagiovanni@gmail.com>
2024-09-02 12:48:24 +0100

    [data-pipeline] better hook logging

--- apps/data-pipeline/data_pipeline/hooks/api_hooks.py
+++ apps/data-pipeline/data_pipeline/hooks/api_hooks.py
@@ -8,10 +8,10 @@ from dagster import (
 @success_hook(name="notify_api_on_success")
 def notify_api_on_success(context: HookContext):
     if context.op.tags.get("hook") == "notify_api_on_success":
-        context.log.info(
-            f"Notifying API on success for {context._step_execution_context.partition_key}"
-        )
-        requests.post(
+        response = requests.post(
             "https://api.enclaveid.com/webhooks/pipeline-finished",
             json={"userId": context._step_execution_context.partition_key},
+        )
+        context.log.info(
+            f"Notified API on success for {context._step_execution_context.partition_key}: {response.text}",
         )
