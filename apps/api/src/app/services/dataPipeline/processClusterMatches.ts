3bd38c215668fbf64210b3fd9e8cc99167b647fc
ma9o <magogagiovanni@gmail.com>
2024-09-02 15:58:38 +0100

    [api] db tx timeouts

--- apps/api/src/app/services/dataPipeline/processClusterMatches.ts
+++ apps/api/src/app/services/dataPipeline/processClusterMatches.ts
@@ -17,120 +17,126 @@ interface ClusterData {
 export async function processClusterMatches(dataFrame: DataFrame) {
   const userId = dataFrame.getColumn('userId').get(0) as string;
 
-  await prisma.$transaction(async (tx) => {
-    // Update or create UserInterests
-    const userInterests = await tx.userInterests.upsert({
-      where: { userId: userId },
-      update: {},
-      create: { userId: userId },
-    });
-
-    // Process clusters and matches
-    const clusterData: ClusterData[] = dataFrame
-      .drop(['userId'])
-      .toRecords()
-      .map((r) => ({
-        interestsCluster: {
-          pipelineClusterId: r.pipelineClusterId,
-          clusterType: r.clusterType,
-          summary: r.summary,
-          title: r.title,
-          isSensitive: r.isSensitive,
-          activityDates: r.activityDates.map(
-            (date) => date.toISOString().split('T')[0],
-          ),
-          clusterItems: r.clusterItems,
-          socialLikelihood: r.socialLikelihood,
-        },
-        interestsClustersSimilarity: r.cosineSimilarity
-          ? {
-              cosineSimilarity: r.cosineSimilarity,
-              commonSummary: r.commonSummary,
-              commonTitle: r.commonTitle,
-              averageSocialLikelihood: r.averageSocialLikelihood,
-            }
-          : undefined,
-        other_user_id: r.other_user_id,
-        other_user_cluster_label: r.other_user_cluster_label,
-      })) as ClusterData[];
-
-    for (const cluster of clusterData) {
-      // Update or create InterestsCluster
-      const interestsCluster = await tx.interestsCluster.upsert({
-        where: {
-          userInterestsId_pipelineClusterId_clusterType: {
-            userInterestsId: userInterests.id,
-            pipelineClusterId: cluster.interestsCluster.pipelineClusterId,
-            clusterType: cluster.interestsCluster.clusterType,
-          },
-        },
-        update: {
-          ...cluster.interestsCluster,
-        },
-        create: {
-          ...cluster.interestsCluster,
-          userInterests: {
-            connect: { id: userInterests.id },
-          },
-        },
+  await prisma.$transaction(
+    async (tx) => {
+      // Update or create UserInterests
+      const userInterests = await tx.userInterests.upsert({
+        where: { userId: userId },
+        update: {},
+        create: { userId: userId },
       });
 
-      // Process match if it exists
-      if (cluster.other_user_id && cluster.other_user_cluster_label) {
-        const otherUserInterests = await tx.userInterests.findUnique({
-          where: { userId: cluster.other_user_id },
+      // Process clusters and matches
+      const clusterData: ClusterData[] = dataFrame
+        .drop(['userId'])
+        .toRecords()
+        .map((r) => ({
+          interestsCluster: {
+            pipelineClusterId: r.pipelineClusterId,
+            clusterType: r.clusterType,
+            summary: r.summary,
+            title: r.title,
+            isSensitive: r.isSensitive,
+            activityDates: r.activityDates.map(
+              (date) => date.toISOString().split('T')[0],
+            ),
+            clusterItems: r.clusterItems,
+            socialLikelihood: r.socialLikelihood,
+          },
+          interestsClustersSimilarity: r.cosineSimilarity
+            ? {
+                cosineSimilarity: r.cosineSimilarity,
+                commonSummary: r.commonSummary,
+                commonTitle: r.commonTitle,
+                averageSocialLikelihood: r.averageSocialLikelihood,
+              }
+            : undefined,
+          other_user_id: r.other_user_id,
+          other_user_cluster_label: r.other_user_cluster_label,
+        })) as ClusterData[];
+
+      for (const cluster of clusterData) {
+        // Update or create InterestsCluster
+        const interestsCluster = await tx.interestsCluster.upsert({
+          where: {
+            userInterestsId_pipelineClusterId_clusterType: {
+              userInterestsId: userInterests.id,
+              pipelineClusterId: cluster.interestsCluster.pipelineClusterId,
+              clusterType: cluster.interestsCluster.clusterType,
+            },
+          },
+          update: {
+            ...cluster.interestsCluster,
+          },
+          create: {
+            ...cluster.interestsCluster,
+            userInterests: {
+              connect: { id: userInterests.id },
+            },
+          },
         });
 
-        if (otherUserInterests) {
-          const otherInterestsCluster = await tx.interestsCluster.findFirst({
-            where: {
-              userInterestsId: otherUserInterests.id,
-              pipelineClusterId: cluster.other_user_cluster_label,
-            },
+        // Process match if it exists
+        if (cluster.other_user_id && cluster.other_user_cluster_label) {
+          const otherUserInterests = await tx.userInterests.findUnique({
+            where: { userId: cluster.other_user_id },
           });
 
-          if (otherInterestsCluster) {
-            // Find existing InterestsClustersSimilarity
-            const existingSimilarity =
-              await tx.interestsClustersSimilarity.findFirst({
-                where: {
-                  interestsClusterMatches: {
-                    every: {
-                      interestsClusterId: {
-                        in: [interestsCluster.id, otherInterestsCluster.id],
+          if (otherUserInterests) {
+            const otherInterestsCluster = await tx.interestsCluster.findFirst({
+              where: {
+                userInterestsId: otherUserInterests.id,
+                pipelineClusterId: cluster.other_user_cluster_label,
+              },
+            });
+
+            if (otherInterestsCluster) {
+              // Find existing InterestsClustersSimilarity
+              const existingSimilarity =
+                await tx.interestsClustersSimilarity.findFirst({
+                  where: {
+                    interestsClusterMatches: {
+                      every: {
+                        interestsClusterId: {
+                          in: [interestsCluster.id, otherInterestsCluster.id],
+                        },
                       },
                     },
                   },
-                },
-              });
+                });
 
-            if (existingSimilarity) {
-              // Update existing InterestsClustersSimilarity
-              await tx.interestsClustersSimilarity.update({
-                where: { id: existingSimilarity.id },
-                data: {
-                  ...cluster.interestsClustersSimilarity,
-                },
-              });
-            } else {
-              // Create new InterestsClustersSimilarity and InterestsClusterMatch
-              await tx.interestsClustersSimilarity.create({
-                data: {
-                  ...cluster.interestsClustersSimilarity,
-                  interestsClusterMatches: {
-                    create: [
-                      { interestsClusterId: interestsCluster.id },
-                      { interestsClusterId: otherInterestsCluster.id },
-                    ],
+              if (existingSimilarity) {
+                // Update existing InterestsClustersSimilarity
+                await tx.interestsClustersSimilarity.update({
+                  where: { id: existingSimilarity.id },
+                  data: {
+                    ...cluster.interestsClustersSimilarity,
                   },
-                },
-              });
+                });
+              } else {
+                // Create new InterestsClustersSimilarity and InterestsClusterMatch
+                await tx.interestsClustersSimilarity.create({
+                  data: {
+                    ...cluster.interestsClustersSimilarity,
+                    interestsClusterMatches: {
+                      create: [
+                        { interestsClusterId: interestsCluster.id },
+                        { interestsClusterId: otherInterestsCluster.id },
+                      ],
+                    },
+                  },
+                });
+              }
             }
           }
         }
       }
-    }
-  });
+    },
+    {
+      timeout: 20000,
+      maxWait: 20000,
+    },
+  );
 
   // Update all the userMatches for the user
   return await processUserMatches(userId);
